cd0a76b3678b1271e9741203f849647c21aaaaa7
Rodeostar42 <rodeostar42@gmail.com>
2018-09-14 01:42:41 +0900

    dwdwdwd

--- dialog/player_inv.hpp
+++ dialog/player_inv.hpp
@@ -276,30 +276,17 @@ class playerSettings {
             h = (1 / 25);
         };
 
-<<<<<<< HEAD
-         class ButtonGovernement : Life_RscButtonInvisible
-       {
-           idc = 2107;
-           onbuttonclick = "closeDialog 0; [] spawn max_gouvernement_fnc_selectGovernementMenu;";
-           text = "";
-           x = 0.657 * safezoneW + safezoneX;
-           y = 0.658 * safezoneH + safezoneY;
-           w = 0.04 * safezoneW;
-           h = 0.067 * safezoneH;
-       };
-       class BoutonGovernement : Life_RscPicture
-       {
-=======
+
         class GovernementMenu: Life_RscButtonMenu {
-            idc = -1;
+            idc = 2107;
             //shortcuts[] = {0x00050000 + 2};
-            text = "$STR_PM_GovernementMenu";
+            text = "GovernementMenu";
             onButtonClick = "[] spawn max_gouvernement_fnc_selectGovernementMenu;"; 
             x = 0.1;
             y = 0.805;
             w = (6.25 / 40);
             h = (1 / 25);
         };
->>>>>>> parent of 085ca79... dwdwdwwd
+
     };
 };
